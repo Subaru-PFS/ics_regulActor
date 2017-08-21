3f48e999f2657562d2975a8d51e418234c306132
Arnaud Le Fur <alefur@lam.fr>
2017-08-21 13:09:29 +0000

    tempLoop pull out from main.py

--- python/regulActor/main.py
+++ python/regulActor/main.py
@@ -1,58 +1,36 @@
 #!/usr/bin/env python
 
-from datetime import datetime as dt
-
 from actorcore.Actor import Actor
-from actorcore.QThread import QThread
-from regulActor.utils import getMean
+from regulActor.temploop import TempLoop
 
 
 class RegulActor(Actor):
     def __init__(self, name, productName=None, configFile=None, debugLevel=30):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        self.cu = "r0"
         Actor.__init__(self, name,
                        productName=productName,
-                       configFile=configFile, modelNames=['xcu_%s' % self.cu])
+                       configFile=configFile, modelNames=['xcu_r0',
+                                                          'xcu_r1',
+                                                          ])
 
-        self.allThreads = {}
-        self.t0 = dt.now()
-        self.period = 600
-        self.kp = 1.
-        self.setpoint = None
-        self.loopOn = False
-        controlLoop = QThread(self, "controlLoop")
-        controlLoop.handleTimeout = self.manageLoop
-        self.allThreads["controlLoop"] = controlLoop
-        controlLoop.start()
+        self.threads = {}
 
-    def startLoop(self, setpoint, period, kp):
-        self.setpoint = setpoint
-        self.period = period
-        self.kp = kp
-        self.loopOn = True
-        self.regulCooler()
+    def startLoop(self, xcu, setpoint, period, kp):
+        temploop = self.threads[xcu] if xcu in self.threads.iterkeys() else TempLoop(self, xcu)
+        temploop.startLoop(setpoint, period, kp)
+        self.threads[xcu] = temploop
 
-    def stopLoop(self):
-        self.loopOn = False
+    def stopLoop(self, xcu):
+        temploop = self.threads[xcu] if xcu in self.threads.iterkeys() else TempLoop(self, xcu)
+        temploop.stopLoop()
+        self.threads[xcu] = temploop
 
-    def manageLoop(self):
-        xcuKeys = self.actor.models['xcu_%s' % self.cu]
-        [setpoint, reject, tip, power] = xcuKeys.keyVarDict['coolerTemps'].getValue()
-        if (dt.now() - self.t0).total_seconds() > self.period and self.loopOn:
-            self.regulCooler()
-
-    def regulCooler(self):
-        self.t0 = dt.now()
-        detector = getMean("xcu_%s__temps" % self.cu, "val1_0")
-        tip = getMean("xcu_%s__coolertemps" % self.cu, "tip")
-        new_tip = tip + self.kp * (self.setpoint - detector)
-
-        self.safeCall(actor='xcu_%s' % self.cu, cmdStr="cooler on setpoint=%.2f" % new_tip, timeLim=60)
+    def status(self, cmd):
+        for i, (xcu, looptemp) in enumerate(self.threads.iteritems()):
+            cmd.inform('loopTemp%i=%s' % (i, looptemp.getStatus()))
 
     def safeCall(self, **kwargs):
-
         cmd = self.bcast
         cmdVar = self.cmdr.call(**kwargs)
 
